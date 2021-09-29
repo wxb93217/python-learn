6af4f87bd2ba0611559b1587d6057f9795550e27
曉彬，😈 <oncwnuK1vRv18xtZ3Iirqtgt9Fvs@git.weixin.qq.com>
2021-09-29 18:31:20 +0800

    迭代

---
+++ 迭代.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from collections.abc import Iterable
+
+def findMinAndMax(L):
+    MIN=0
+    MAX=0
+    if isinstance(L, Iterable) and L:
+        for i, value in enumerate(L):
+            if i==0:
+                MIN=value
+                MAX=value
+            elif value>=MAX:
+                MAX=value
+            elif value<=MIN:
+                MIN=value
+            else:
+                pass
+            #print(i,value)
+        L=(MIN, MAX)
+    else:
+        L=(None, None)
+    print(L)
+    return L
+# 测试
+if findMinAndMax([]) != (None, None):
+    print('测试失败1!')
+elif findMinAndMax([7]) != (7, 7):
+    print('测试失败2!')
+elif findMinAndMax([7, 1]) != (1, 7):
+    print('测试失败3!')
+elif findMinAndMax([7, 1, 3, 9, 5]) != (1, 9):
+    print('测试失败4!')
+else:
+    print('测试成功!')
